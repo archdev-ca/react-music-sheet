811b3d07f8ae7da5d659591276516766ca354748
acgelonga <acgelonga@gmail.com>
2023-07-12 14:23:18 -0500

    Prepare initial notes images

--- src/components/BarLine.tsx
+++ src/components/BarLine.tsx
@@ -4,17 +4,28 @@ interface Props {
   isFloating?: boolean;
 }
 
-const StyledBarLine = styled("div")`
+const StyledBarLine = styled("div")(({ theme }) => {
+  return {
+    padding: "4px 0",
+    "&:hover > div": {
+      backgroundColor: `${theme.palette.primary.plainHoverBg} !important`,
+    },
+  };
+});
+
+const Line = styled("div")`
   height: 2px;
 `;
 
 const BarLine = ({ isFloating }: Props) => {
   return (
-    <StyledBarLine
-      style={{
-        backgroundColor: isFloating ? "transparent" : "#000",
-      }}
-    ></StyledBarLine>
+    <StyledBarLine>
+      <Line
+        style={{
+          backgroundColor: isFloating ? "transparent" : "#000",
+        }}
+      ></Line>
+    </StyledBarLine>
   );
 };
 
--- src/pages/sheet/create.tsx
+++ src/pages/sheet/create.tsx
@@ -1,10 +1,68 @@
 import Staff from "@/components/Staff";
+import { Box, Card, CardContent, IconButton } from "@mui/joy";
+import wholeNote from "@/assets/notes/whole.png";
+import halfNote from "@/assets/notes/half.png";
+import quarterNote from "@/assets/notes/quarter.png";
+import eightNote from "@/assets/notes/eighth.png";
+import sixteenthNote from "@/assets/notes/sixteenth.png";
 
 const SheetCreate = () => {
   return (
     <>
       <h2 className="text-2xl">Create Sheet Music</h2>
-      <Staff />
+      <Card>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+          }}
+        >
+          <IconButton>
+            <img
+              src={wholeNote}
+              alt=""
+              style={{ width: "auto", height: " 10px" }}
+            />
+          </IconButton>
+          <IconButton>
+            <img
+              src={halfNote}
+              alt=""
+              style={{ width: "auto", height: " 28px" }}
+            />
+          </IconButton>
+          <IconButton>
+            <img
+              src={quarterNote}
+              alt=""
+              style={{ width: "auto", height: " 28px" }}
+            />
+          </IconButton>
+          <IconButton>
+            <img
+              src={eightNote}
+              alt=""
+              style={{ width: "auto", height: " 28px" }}
+            />
+          </IconButton>
+          <IconButton>
+            <img
+              src={sixteenthNote}
+              alt=""
+              style={{ width: "auto", height: " 28px" }}
+            />
+          </IconButton>
+        </Box>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+          }}
+        ></Box>
+        <Staff />
+      </Card>
     </>
   );
 };
