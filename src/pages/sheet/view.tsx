5891fe663edbadfee2b6ef2f4ef58c73393393b2
acgelonga <acgelonga@gmail.com>
2023-10-16 13:19:41 -0500

    Add breadcrumb title

--- src/pages/sheet/view.tsx
+++ src/pages/sheet/view.tsx
@@ -1,36 +1,14 @@
 import useAppStore from "@/store/app";
 import useSheetStore from "@/store/sheet";
-import { Add, Home, Visibility } from "@mui/icons-material";
-import {
-  Box,
-  Breadcrumbs,
-  Button,
-  Card,
-  Link,
-  Stack,
-  Table,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Home } from "@mui/icons-material";
+import { Box, Breadcrumbs, Card, Link, Stack, Typography } from "@mui/material";
 import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const SheetIndex = () => {
   const { id } = useParams<"id">();
-  const navigate = useNavigate();
   const titles = useAppStore((state) => state.titles);
   const { data, setData } = useSheetStore((state) => state);
-
-  const handleClickCreate = () => {
-    navigate("/create");
-  };
-
-  const handleClickView = (id: number) => {
-    navigate(`/view/${id}`);
-  };
 
   useEffect(() => {
     if (id !== undefined && titles[Number(id)]) {
@@ -46,6 +24,7 @@ const SheetIndex = () => {
             <Home fontSize="inherit" sx={{ mr: 0.5 }} />
             Music Sheets
           </Link>
+          {data && data.title ? <Typography>{data.title}</Typography> : null}
         </Breadcrumbs>
         <Stack
           alignContent="center"
@@ -54,16 +33,7 @@ const SheetIndex = () => {
           justifyContent="space-between"
           marginBottom={2}
         >
-          <Typography variant="h4">Music Sheets</Typography>
-          <Button
-            onClick={handleClickCreate}
-            sx={{
-              alignSelf: "center",
-            }}
-          >
-            <Add sx={{ mr: 0.5 }} fontSize="inherit" />
-            Create New Sheet
-          </Button>
+          <Typography variant="h4">View Music</Typography>
         </Stack>
         <Card>Some Stuff</Card>
       </Box>
