47e5e1a1fb6e3e6b602d6a2378cb649c7848cfd9
acgelonga <acgelonga@gmail.com>
2023-10-24 22:51:52 -0500

    TODO: props.theme error no palette

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // emotion: true,
+    emotion: {
+      sourceMap: true,
+      autoLabel: "dev-only",
+      labelFormat: "[local]",
+      importMap: {
+        "@mui/material": {
+          styled: {
+            canonicalImport: ["@emotion/styled", "default"],
+            styledBaseImport: ["@mui/material", "styled"],
+          },
+        },
+        "@mui/material/styles": {
+          styled: {
+            canonicalImport: ["@emotion/styled", "default"],
+            styledBaseImport: ["@mui/material/styles", "styled"],
+          },
+        },
+      },
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,31 +1,48 @@
-import { styled } from "@mui/material";
+import { styled as mstyled } from "@mui/material";
+import styled from "@emotion/styled";
 
 type StyleSpaceProps = {
   line?: boolean;
   ornamental?: boolean;
 };
 
-const StyledLine = styled("div")`
+const StyledLine = styled.div`
   height: 1px;
   background-color: #000;
 `;
-const StyledFloatingLine = styled("div")`
+const StyledLine2 = mstyled("div")`
+  height: 1px;
+  background-color: #000;
+`;
+const StyledFloatingLine = mstyled("div")`
   height: 1px;
   background-color: rgba(0, 0, 0, 0.1);
 `;
 
-const StyledSpace = styled("div")<StyleSpaceProps>(
-  ({ theme, ornamental }) => `
+const StyledSpace = styled.div<StyleSpaceProps>`
   height: 9px;
   padding: 4px 0;
+  transition: background-color 0.3s ease;
   &:hover {
-    background-color: ${ornamental ? "inherit" : theme.palette.primary.light};
+    background-color: ${(props) =>
+      props.ornamental ? "inherit" : props.theme.palette.primary.light};
   }
   &:hover > ${StyledLine} {
-    background-color: #f00 !important;
+    background-color: #fff;
   }
-`
-);
+`;
+
+const StyledSpace2 = mstyled("div")`
+  height: 9px;
+  padding: 4px 0;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #00f;
+  }
+  &:hover > ${StyledLine2} {
+    background-color: #fff;
+  }
+`;
 
 type Props = {
   line?: boolean;
@@ -35,10 +52,12 @@ type Props = {
 
 function Space({ line, floating, ornamental }: Props) {
   return (
-    <StyledSpace ornamental={ornamental}>
-      {!floating && line ? <StyledLine /> : null}
-      {floating && line ? <StyledFloatingLine /> : null}
-    </StyledSpace>
+    <>
+      <StyledSpace2 ornamental={ornamental}>
+        {!floating && line ? <StyledLine /> : null}
+        {floating && line ? <StyledFloatingLine /> : null}
+      </StyledSpace2>
+    </>
   );
 }
 
